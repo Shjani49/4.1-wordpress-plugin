02d6e6afa23262b96c130c9f89994107391a2848
Shjani49 <shjani49@gmail.com>
2020-08-17 13:58:44 -0600

    Initial commit; add plugin file.

---
+++ techcareerscalculator/techcareerscalculator.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: TECHCareer Calculator
+Plugin URI: http://TECHCareers.ca/
+Description : An example plugin for the TECHCareers class. It provides an example of a WordPress short code. Type [techcareerscalculator] into a page or post to output our calculator.
+Author: Shivani
+Version: 1.0
+Author URI: http://ShivaniJani.ca
+*/
